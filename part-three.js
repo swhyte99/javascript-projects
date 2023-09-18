1b67191a5514d88fd7c7f0d49889e0d5e17ee792
Sam Whyte <142351152+swhyte99@users.noreply.github.com>
2023-09-18 13:58:16 -0500

    Add files via upload

---
+++ part-three.js
@@ -0,0 +1,19 @@
+//Part Three section one
+
+let language = 'JavaScript';
+let initials = language.slice(0,1)+language.slice(4,5);
+
+//1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
+console.log(language.slice(0,1)+language.slice(4,5));
+//2. Without using slice(), use method chaining to accomplish the same thing.
+console.log(language.replace("JavaScript", "JS"));
+//3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+console.log(`The abbreviation for '${language}' is '${initials}'`);
+//4. Just for fun, try chaining 3 or more methods together, and then print the result.
+
+//Part Three section Two
+
+//1. Use the string methods you know to print 'Title Case' from the string 'title case'.
+
+let notTitleCase = 'title case';
+console.log(notTitleCase.replace("title case", "Title Case"));
